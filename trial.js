4046f6555a241aa7dc120648505e504a85def5e8
Prakshi <prakshijain198@gmail.com>
2020-12-13 08:53:29 +0000

    fix: removed unused files

--- trial.js
+++
@@ -1,6 +0,0 @@
-var readlineSync = require('readline-sync');
-
-  animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo']
-
-index = readlineSync.keyInSelect(animals, 'Which animal?');
-console.log('Ok,' + animals[index] +' goes to your room.');
